d4aa9656a25539eebcdc42d9d1c19315f63100e3
pszmk <przemyslawpietrzak2001@gmail.com>
2024-05-26 19:25:32 +0200

    chore: Update .gitignore to include new directories

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # D4L-Hackaton
+
+### Viewing logs
+To see the logs from different runs from the `src` directory run
+
+```bash
+tensorboard --logdir=logs
+```
--- src/utils/paths.py
+++ src/utils/paths.py
@@ -6,7 +6,10 @@ ANNDATA_FILENAME: str = "GSE194122_openproblems_neurips2021_cite_BMMC_processed.
 ANNDATA_PATH: Path = DATA_PATH / ANNDATA_FILENAME
 
 # Config paths
-CONFIG_PATH: Path = Path(__file__).parent / "config"
+CONFIG_PATH: Path = Path(__file__).parent.parent / "config"
 
 # Results paths
 RESULTS_PATH: Path = Path(__file__).parent.parent / "results"
+
+# Logs paths
+LOGS_PATH: Path = Path(__file__).parent.parent / "logs"
