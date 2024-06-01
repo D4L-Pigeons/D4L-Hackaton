31724d3dac01d32975d4e2f6ab5865ba1b288b54
pszmk <przemyslawpietrzak2001@gmail.com>
2024-06-01 15:52:16 +0200

    Refactor omivae file. Half-way to gmm_iwae or more.

--- src/models/omivae.py
+++ src/models/omivae.py
@@ -10,18 +10,21 @@ from utils.paths import LOGS_PATH
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import torch.distributions as td
 import torch.optim as optim
 import pytorch_lightning as pl
+from sklearn.metrics import balanced_accuracy_score
 from argparse import Namespace
 import anndata as ad
 from anndata import AnnData
-from typing import Tuple
+from typing import Tuple, Dict, Optional, Union
 from torch import Tensor
+from abc import ABC, abstractmethod
 
 
-class OmiVAEGaussian(pl.LightningModule):
+class OmiAE(pl.LightningModule):
     def __init__(self, cfg: Namespace):
-        super(OmiVAEGaussian, self).__init__()
+        super(OmiAE, self).__init__()
         self.assert_cfg(cfg)
         self.cfg = cfg
         self.fstmod_in = ShortcutBlock(
@@ -40,30 +43,30 @@ class OmiVAEGaussian(pl.LightningModule):
             Block(
                 input_size=cfg.first_modality_embedding_dim
                 + cfg.second_modality_embedding_dim,
-                output_size=cfg.latent_dim * 2,
-                hidden_size=cfg.hidden_dim,
+                output_size=cfg.encoder_out_dim,
+                hidden_size=cfg.encoder_hidden_dim,
                 batch_norm=cfg.batch_norm,
             ),
             nn.SiLU(),
             ResidualBlock(
-                input_size=cfg.latent_dim * 2,
-                hidden_size=cfg.hidden_dim,
+                input_size=cfg.encoder_out_dim,
+                hidden_size=cfg.encoder_hidden_dim,
                 batch_norm=cfg.batch_norm,
             ),
         )
         self.decoder = nn.Sequential(
             ShortcutBlock(
-                input_size=cfg.latent_dim,
-                output_size=cfg.latent_dim * 2,
-                hidden_size=cfg.hidden_dim,
+                input_size=cfg.decoder_in_dim,
+                output_size=cfg.decoder_in_dim * 2,
+                hidden_size=cfg.decoder_hidden_dim,
                 batch_norm=cfg.batch_norm,
             ),
             nn.SiLU(),
             Block(
-                input_size=cfg.latent_dim * 2,
+                input_size=cfg.decoder_in_dim * 2,
                 output_size=cfg.first_modality_embedding_dim
                 + cfg.second_modality_embedding_dim,
-                hidden_size=cfg.hidden_dim,
+                hidden_size=cfg.decoder_hidden_dim,
                 batch_norm=cfg.batch_norm,
             ),
         )
@@ -81,7 +84,7 @@ class OmiVAEGaussian(pl.LightningModule):
         )
         if cfg.classification_head:
             self.classification_head = Block(
-                input_size=cfg.latent_dim,
+                input_size=cfg.decoder_in_dim,
                 output_size=cfg.num_classes,
                 hidden_size=cfg.num_classes * 2,
                 batch_norm=cfg.batch_norm,
@@ -89,9 +92,12 @@ class OmiVAEGaussian(pl.LightningModule):
 
     def _encode(self, x_fst: Tensor, x_snd: Tensor) -> Tensor:
         x_fst = self.fstmod_in(x_fst)
+        print("encode 0 passed")
         x_snd = self.sndmod_in(x_snd)
-        mu, logvar = self.encoder(torch.cat([x_fst, x_snd], dim=1)).chunk(2, dim=1)
-        return mu, logvar
+        print("encode 1 passed")
+        encoder_out = self.encoder(torch.cat([x_fst, x_snd], dim=-1))
+
+        return encoder_out
 
     def _decode(self, z: Tensor) -> Tuple[Tensor]:
         x_fst, x_snd = self.decoder(z).split(
@@ -99,90 +105,87 @@ class OmiVAEGaussian(pl.LightningModule):
                 self.cfg.first_modality_embedding_dim,
                 self.cfg.second_modality_embedding_dim,
             ],
-            dim=1,
+            dim=-1,
         )
+        print("decode 0 passed")
         x_fst = self.fstmod_out(x_fst)
+        print("decode 1 passed")
         x_snd = self.sndmod_out(x_snd)
+        print("decode 2 passed")
+
         return x_fst, x_snd
 
-    def _get_decoder_jacobian(self, z: Tensor) -> Tensor:
-        return torch.autograd.functional.jacobian(self.decoder, z)
+    def _classification_processing(
+        self, latent_representation: Tensor, y: Tensor, compute_accuracy: bool
+    ) -> Tuple[Tensor, Optional[Tensor], Optional[Tensor]]:
+        logits = self.classification_head(latent_representation)
+        c_loss = F.cross_entropy(logits, y, weight=self.cfg.class_weights)
+        if compute_accuracy:
+            acc = ((torch.argmax(logits, dim=1) == y).float().mean()).item()
+            bac = balanced_accuracy_score(
+                y.cpu().numpy(), torch.argmax(logits, dim=1).cpu().numpy()
+            )
+            return c_loss, acc, bac
 
-    def _reparameterize(self, mu: Tensor, logvar: Tensor) -> Tensor:
-        std = torch.exp(0.5 * logvar)
-        eps = torch.randn(std.size())
-        return mu + eps * std
+        return c_loss, None, None
 
-    def _forward_usupervised(self, x_fst: Tensor, x_snd: Tensor) -> Tuple[Tensor]:
-        mu, logvar = self._encode(x_fst, x_snd)
-        z = self._reparameterize(mu, logvar)
-        x_fst_hat, x_snd_hat = self._decode(z)
-        return x_fst_hat, x_snd_hat, mu, logvar
-
-    def _training_step_unsupervised(self, batch: Tensor) -> Tensor:
-        x_fst, x_snd = batch
-        x_fst_hat, x_snd_hat, mu, logvar = self._forward_usupervised(x_fst, x_snd)
-        recon_loss = F.mse_loss(x_fst_hat, x_fst) + F.mse_loss(x_snd_hat, x_snd)
-        kld_loss = kld_stdgaussian(mu, logvar)
-
-        return recon_loss, kld_loss
-
-    def _forward_superivsed(self, x_fst: Tensor, x_snd: Tensor) -> Tuple[Tensor]:
-        x_fst_hat, x_snd_hat, mu, logvar = self._forward_usupervised(x_fst, x_snd)
-        logits = self.classification_head(mu)  # classification performed with mu
-        return x_fst_hat, x_snd_hat, logits, mu, logvar
-
-    def _training_step_supervised(self, batch: Tensor) -> Tensor:
-        x_fst, x_snd, target = batch
-        x_fst_hat, x_snd_hat, logits, mu, logvar = self._forward_superivsed(
-            x_fst, x_snd
+    def forward(
+        self, batch: Tuple[Tensor], compute_accuracy: bool = False
+    ) -> Tuple[Tensor, Dict[str, float]]:
+        metrics = {}
+        total_loss = 0.0
+        x_fst, x_snd, *packed_labels_potentially = batch
+        assert isinstance(x_fst, Tensor), TypeError(
+            f"x_fst must be a Tensor, got {type(x_fst)} instead."
         )
-        recon_loss = gex_reconstruction_loss(
-            x_fst_hat, x_fst
-        ) + adt_reconstruction_loss(x_snd_hat, x_snd)
-        kld_loss = kld_stdgaussian(mu, logvar)
-        c_loss = F.cross_entropy(logits, target, weight=self.cfg.class_weights)
+        assert isinstance(x_snd, Tensor), TypeError(
+            f"x_snd must be a Tensor, got {type(x_snd)} instead."
+        )
+        z = self._encode(x_fst, x_snd)
+        assert isinstance(z, Tensor), TypeError(
+            f"z must be a Tensor, got {type(z)} instead."
+        )
+        x_fst_hat, x_snd_hat = self._decode(z)
+        assert isinstance(x_fst_hat, Tensor), TypeError(
+            f"x_fst_hat must be a Tensor, got {type(x_fst_hat)} instead."
+        )
+        assert isinstance(x_snd_hat, Tensor), TypeError(
+            f"x_snd_hat must be a Tensor, got {type(x_snd_hat)} instead."
+        )
+        recon_loss = F.mse_loss(x_fst_hat, x_fst) + F.mse_loss(x_snd_hat, x_snd)
+        metrics["recon_loss"] = recon_loss.item()
+        total_loss += self.cfg.recon_loss_coef * recon_loss
 
-        return recon_loss, kld_loss, c_loss
+        if self.cfg.classification_head:
+            c_loss, acc, bac = self._classification_processing(
+                z, *packed_labels_potentially, compute_accuracy
+            )
+            metrics["class_loss"] = c_loss.item()
+            if acc is not None:
+                metrics["acc"] = acc
+                metrics["bac"] = bac
+            total_loss += self.cfg.c_loss_coef * c_loss
+
+        return total_loss, metrics
 
     def training_step(self, batch: Tensor) -> Tensor:
-        if self.cfg.classification_head:
-            recon_loss, kld_loss, c_loss = self._training_step_supervised(batch)
-            self.log("Train recon", recon_loss, on_epoch=True, prog_bar=True)
-            self.log("Train kld", kld_loss, on_epoch=True, prog_bar=True)
-            self.log("Train class", c_loss, on_epoch=True, prog_bar=True)
-            # print("Train recon", recon_loss)
-            return (
-                self.cfg.recon_loss_coef * recon_loss
-                + self.cfg.kld_loss_coef * kld_loss
-                + self.cfg.c_loss_coef * c_loss
-            )
-        else:
-            recon_loss, kld_loss = self._training_step_unsupervised(batch)
-            self.log("Train recon", recon_loss, on_epoch=True, prog_bar=True)
-            self.log("Train kld", kld_loss, on_epoch=True, prog_bar=True)
-            # print("Train recon", recon_loss)
-            return (
-                self.cfg.recon_loss_coef * recon_loss
-                + self.cfg.kld_loss_coef * kld_loss
-            )
+        loss, loss_components = self(batch)
+        self.log("Train loss", loss, on_epoch=True, prog_bar=True)
+        for k, v in loss_components.items():
+            self.log(f"Train {k}", v, on_epoch=True, prog_bar=True)
 
     def validation_step(self, batch: Tensor) -> Tensor:
-        if self.cfg.classification_head:
-            recon_loss, kld_loss, c_loss = self._training_step_supervised(batch)
-            self.log("Val recon", recon_loss, on_epoch=True, prog_bar=True)
-            self.log("Val kld", kld_loss, on_epoch=True, prog_bar=True)
-            self.log("Val class", c_loss, on_epoch=True, prog_bar=True)
-        else:
-            recon_loss, kld_loss = self._training_step_unsupervised(batch)
-            self.log("Val recon", recon_loss, on_epoch=True, prog_bar=True)
-            self.log("Val kld", kld_loss, on_epoch=True, prog_bar=True)
+        loss, loss_components = self(batch)
+        self.log("Val loss", loss, on_epoch=True, prog_bar=True)
+        for k, v in loss_components.items():
+            self.log(f"Val {k}", v, on_epoch=True, prog_bar=True)
 
     def _predict(self, x: Tensor) -> Tensor:
         if not self.classification_head:
             raise ValueError("Model does not have a classification head")
         mu, _ = self._encode(x)
         logits = self.classification_head(mu)
+
         return logits
 
     def _predict_proba(self, data: AnnData):
@@ -191,6 +194,9 @@ class OmiVAEGaussian(pl.LightningModule):
         mu, _ = self._encode(data)
         logits = self.classification_head(mu)
         return torch.softmax(logits, dim=1)
+
+    def _get_decoder_jacobian(self, z: Tensor) -> Tensor:
+        return torch.autograd.functional.jacobian(self.decoder, z)
 
     def configure_optimizers(self):
         return optim.Adam(self.parameters(), lr=self.cfg.lr)
@@ -201,12 +207,6 @@ class OmiVAEGaussian(pl.LightningModule):
         )
         assert hasattr(cfg, "log_every_n_steps"), AttributeError(
             'cfg does not have the attribute "log_every_n_steps"'
-        )
-        # assert hasattr(cfg, "logger"), AttributeError(
-        #     'cfg does not have the attribute "logger"'
-        # )
-        assert hasattr(cfg, "classification_head"), AttributeError(
-            'cfg does not have the attribute "classification_head"'
         )
         assert hasattr(cfg, "first_modality_hidden_dim"), AttributeError(
             'cfg does not have the attribute "first_modality_hidden_dim"'
@@ -220,29 +220,138 @@ class OmiVAEGaussian(pl.LightningModule):
         assert hasattr(cfg, "second_modality_embedding_dim"), AttributeError(
             'cfg does not have the attribute "second_modality_embedding_dim"'
         )
+        assert hasattr(cfg, "encoder_hidden_dim"), AttributeError(
+            'cfg does not have the attribute "encoder_hidden_dim"'
+        )
+        assert hasattr(cfg, "encoder_out_dim"), AttributeError(
+            'cfg does not have the attribute "encoder_out_dim"'
+        )
+        assert hasattr(cfg, "decoder_in_dim"), AttributeError(
+            'cfg does not have the attribute "decoder_in_dim"'
+        )
+        assert hasattr(cfg, "decoder_hidden_dim"), AttributeError(
+            'cfg does not have the attribute "decoder_hidden_dim"'
+        )
         assert hasattr(cfg, "recon_loss_coef"), AttributeError(
             'cfg does not have the attribute "recon_loss_coef"'
         )
         assert hasattr(cfg, "kld_loss_coef"), AttributeError(
             'cfg does not have the attribute "kld_loss_coef"'
         )
-        if cfg.classification_head:
-            assert hasattr(cfg, "num_classes"), AttributeError(
-                'cfg does not have the attribute "num_classes"'
-            )
-            assert hasattr(cfg, "class_weights"), AttributeError(
-                'cfg does not have the attribute "class_weights"'
-            )
-            assert hasattr(cfg, "c_loss_coef"), AttributeError(
-                'cfg does not have the attribute "c_loss_coef"'
-            )
         assert hasattr(cfg, "lr"), AttributeError(
             'cfg does not have the attribute "lr"'
         )
 
 
+class OmiGMPriorProbabilisticAE(OmiAE):
+    def __init__(self, cfg: Namespace):
+        super(OmiGMPriorProbabilisticAE, self).__init__()
+        self.assert_cfg(cfg)
+        self.component_logits = nn.Parameter(
+            data=torch.zeros(size=(cfg.no_components,)), requires_grad=True
+        )
+        self.means = nn.Parameter(
+            torch.randn(cfg.no_components, self.latent_dim), requires_grad=True
+        )
+        # STDs of GMM
+        self.register_buffer(
+            "stds", cfg.components_std * torch.ones(cfg.no_components, self.latent_dim)
+        )
+
+    def _var_transformation(self, logvar: Tensor) -> Tensor:
+        return torch.exp(0.5 * logvar)
+
+    def forward(
+        self, batch: Tuple[Tensor], compute_accuracy: bool = False
+    ) -> Tuple[Tensor]:
+        metrics: Dict[str, float] = {}
+        total_loss: Tensor = 0.0
+        x_fst, x_snd, labels = (
+            batch  # ASSUMPTION THAT ALL LABELS ARE AVAILABLE (the extension to the mix of alebeled + unlabeled is not difficuls, but it is not implemented here as it may not be necessary for the task at hand)
+        )
+
+        z_means, z_stds = self._encode(x_fst, x_snd).chunk(2, dim=1)
+        z_stds = self._var_transformation(z_stds)
+        normal_rv = self._make_normal_rv(z_means, z_stds)
+        entropy_per_batch_sample = normal_rv.entropy().sum(dim=1).unsqueeze(0)  # [1, B]
+        z_sample = normal_rv.rsample(
+            sample_size=(self.cfg.no_latent_samples,)
+        ).unsqueeze(
+            2
+        )  # [K, B, 1, latent_dim]
+
+        gmm = self._make_gmm()
+        per_component_logprob = gmm.component_distribution.log_prob(
+            z_sample
+        )  # [K, B, no_components]
+        gmm_likelihood_per_k = per_component_logprob[:, :, labels]  # [K, B]
+
+        x_fst_hat, x_snd_hat = self._decode(z_sample)
+        recon_loss_per_k = F.mse_loss(
+            x_fst_hat, x_fst.repeat(self.cfg.no_latent_samples, 1, 1), reduction="none"
+        ).mean(dim=-1) + F.mse_loss(
+            x_snd_hat, x_snd.repeat(self.cfg.no_latent_samples, 1, 1), reduction="none"
+        ).mean(
+            dim=-1
+        )  # [K, B]
+
+        if self.cfg.no_latent_samples > 1:  # IWAE with no_latent_samples latent samples
+            total_loss = -torch.logsumexp(
+                self.cfg.gmm_likelihood_coef * gmm_likelihood_per_k
+                + self.cfg.entropy_coef * entropy_per_batch_sample
+                + self.recon_coef * recon_loss_per_k,
+                dim=0,
+            ).mean()
+        else:  # IWAE reduces to VAE with one latent sample
+            total_loss = -(
+                gmm_likelihood_per_k + recon_loss_per_k + entropy_per_batch_sample
+            ).mean()
+
+        metrics["entropy"] = entropy_per_batch_sample.mean().item()
+        metrics["gmm_likelihood"] = gmm_likelihood_per_k.mean().item()
+        metrics["recon_loss"] = recon_loss_per_k.mean().item()
+
+        if self.cfg.classification_head:
+            c_loss, acc, bac = self._classification_processing(
+                z_means, labels, compute_accuracy
+            )
+            metrics["class_loss"] = c_loss.item()
+            if acc is not None:
+                metrics["acc"] = acc
+                metrics["bac"] = bac
+            total_loss += self.cfg.c_loss_coef * c_loss
+
+        return total_loss, metrics
+
+    def _make_normal_rv(self, mu: Tensor, logvar: Tensor):
+        return td.Normal(mu, self._var_transformation(logvar))
+
+    def _make_gmm(self):
+        categorical = td.Categorical(logits=self.component_logits)
+        comp = td.Independent(
+            td.Normal(self.means, self.stds), reinterpreted_batch_ndims=1
+        )
+        return td.MixtureSameFamily(categorical, comp)
+
+    def assert_cfg(self, cfg: Namespace) -> None:
+        super(OmiGMPriorProbabilisticAE, self).assert_cfg(cfg)
+        assert hasattr(cfg, "no_components"), AttributeError(
+            'cfg does not have the attribute "no_components"'
+        )
+        assert hasattr(cfg, "components_std"), AttributeError(
+            'cfg does not have the attribute "components_std"'
+        )
+        assert hasattr(cfg, "gmm_likelihood_coef"), AttributeError(
+            'cfg does not have the attribute "gmm_likelihood_coef"'
+        )
+        assert hasattr(cfg, "entropy_coef"), AttributeError(
+            'cfg does not have the attribute "entropy_coef"'
+        )
+
+
 _OMIVAE_IMPLEMENTATIONS = {
-    "OmiVAEGaussian": OmiVAEGaussian,
+    "OmiAE": OmiAE,
+    # "OmiVAEGaussian": OmiGMPriorProbabilisticAE,
 }
 
 
