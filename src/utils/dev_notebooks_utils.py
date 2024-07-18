6262b71f92c7cd2ca231481f7aa565d5c22c2291
pszmk <przemyslawpietrzak2001@gmail.com>
2024-07-18 18:37:04 +0200

    Add almost whole simple RAE

--- src/utils/dev_notebooks_utils.py
+++ src/utils/dev_notebooks_utils.py
@@ -86,6 +86,44 @@ def dict_to_namespace(d):
     return Namespace(**d)
 
 
+def get_data_embeddings(
+    tensor_dataset: TensorDataset, model, batch_size: int = 1
+) -> Path:
+    r"""
+    Computes the embeddings of the data using the model and saves them to an HDF5 file.
+    """
+    dataloader = DataLoader(tensor_dataset, batch_size=batch_size, shuffle=False)
+    model.eval()
+    # Open an HDF5 file
+    with h5py.File(
+        EMBEDDINGS_PATH / f"{model.cfg.model.model_name}-embeddings.h5", "w"
+    ) as h5f:
+        # Create a resizable dataset to store embeddings
+        max_shape = (
+            None,
+            model.cfg.model.latent_dim,
+        )  # None indicates that the dimension is resizable
+        dset = h5f.create_dataset(
+            "embeddings",
+            shape=(0, model.cfg.model.latent_dim),
+            maxshape=max_shape,
+            dtype="float32",
+        )
+
+        for i, x in enumerate(dataloader):
+            x = x[0]
+            model.to(x.device)
+            with torch.no_grad():
+                encoded = model.encode(x)
+
+            # Resize the dataset to accommodate new embeddings
+            dset.resize(dset.shape[0] + encoded.shape[0], axis=0)
+            # Write the new embeddings
+            dset[-encoded.shape[0] :] = encoded.detach().cpu().numpy()
+
+    return EMBEDDINGS_PATH / f"{model.cfg.model.model_name}-embeddings.h5"
+
+
 def get_data_embeddings_transformer_version(
     tensor_dataset: TensorDataset, model, batch_size: int = 1
 ) -> Path:
@@ -93,7 +131,7 @@ def get_data_embeddings_transformer_version(
     Computes the embeddings of the data using the model and saves them to an HDF5 file.
     """
     dataloader = DataLoader(tensor_dataset, batch_size=batch_size, shuffle=False)
-
+    model.eval()
     # Open an HDF5 file
     with h5py.File(
         EMBEDDINGS_PATH / f"{model.cfg.model.model_name}-embeddings.h5", "w"
@@ -117,7 +155,10 @@ def get_data_embeddings_transformer_version(
                 x, dim=1, index=encoder_input_gene_idxs
             )
             model.to(encoder_input_exprs_lvls.device)
-            _, mu, _ = model.encode(encoder_input_gene_idxs, encoder_input_exprs_lvls)
+            with torch.no_grad():
+                _, mu, _ = model.encode(
+                    encoder_input_gene_idxs, encoder_input_exprs_lvls
+                )
 
             # Resize the dataset to accommodate new embeddings
             dset.resize(dset.shape[0] + mu.shape[0], axis=0)
