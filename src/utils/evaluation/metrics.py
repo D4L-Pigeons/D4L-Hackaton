e1627fbf6b83f0ef5b58db4c4d5aa7351cdf2cb7
asia <jwojciechowska210@gmail.com>
2024-08-11 09:51:36 +0200

    a few changes for good quality code

--- src/utils/evaluation/metrics.py
+++ src/utils/evaluation/metrics.py
@@ -1,8 +1,17 @@
-# calculates desired metrics
-# --- METRICS ---
+from typing import List
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import (
+    adjusted_rand_score,
+    f1_score,
+    normalized_mutual_info_score,
+    silhouette_score,
+)
+from sklearn.neighbors import KNeighborsClassifier
 
 
-def _calc_entropy(preds_probs: ndarray) -> float:
+def _calc_entropy(preds_probs: np.ndarray) -> float:
     r"""
     Calculates the entropy of a distribution.
 
@@ -17,13 +26,20 @@ def _calc_entropy(preds_probs: ndarray) -> float:
 
 
 def calc_metrics_from_uniport(
-    train_embeddings: ndarray,
-    generalization_embeddings: ndarray,
-    train_df: DataFrame,
-    generalization_df: DataFrame,
+    train_embeddings: np.ndarray,
+    generalization_embeddings: np.ndarray,
+    train_df: pd.DataFrame,
+    generalization_df: pd.DataFrame,
     n_neighbors: int = 50,
     n_loc_samples: int = 100,
-) -> DataFrame:
+    metrics_list: List[str] = [
+        "AdjustedRandIndex",
+        "NormalizedMutualInformation",
+        "F1Score",
+        "SilhouetteCoefficient",
+        "MixingEntropyScore",
+    ],
+) -> pd.DataFrame:
     r"""
     Calculates the metrics from the embeddings and the metadata.
 
@@ -34,6 +50,7 @@ def calc_metrics_from_uniport(
         generalization_df (DataFrame): DataFrame containing generalization metadata.
         n_neighbors (int, optional): Number of neighbors to consider for KNN classification. Defaults to 50.
         n_loc_samples (int, optional): Number of samples to select from generalization embeddings. Defaults to 100.
+        metrics_list (List[str], optional): List of metrics to calculate. Defaults to all metrics.
 
     Returns:
         DataFrame: DataFrame containing calculated metrics for each column in train_df.
@@ -41,22 +58,19 @@ def calc_metrics_from_uniport(
     Note:
         AverageFOSCTTM is not calculated.
     """
-    knc = KNeighborsClassifier(n_neighbors=n_neighbors)
 
-    metrics = pd.DataFrame(
-        columns=[
-            "col_name",
-            "AdjustedRandIndex",
-            "NormalizedMutualInformation",
-            "F1Score",
-            "SilhouetteCoefficient",
-            "MixingEntropyScore",
-        ],
-        index=train_df.columns,
+    # Ensure the number of components isn't larger than the data
+    assert (
+        n_loc_samples <= generalization_embeddings.shape[0]
+    ), "n_loc_samples cannot exceed the number of generalization samples."
+
+    knc = KNeighborsClassifier(n_neighbors=n_neighbors)
+    metrics = pd.DataFrame(columns=metrics_list, index=train_df.columns)
+
+    sampled_region_indices = np.random.randint(
+        0, generalization_embeddings.shape[0], n_loc_samples
     )
-    sampled_region_indices = torch.randint(
-        0, generalization_embeddings.shape[0], (n_loc_samples,)
-    )
+
     for col_name in train_df.columns:
         knc.fit(train_embeddings, train_df[col_name])
         labels = generalization_df[col_name]
@@ -64,14 +78,24 @@ def calc_metrics_from_uniport(
         preds_probs = knc.predict_proba(
             generalization_embeddings[sampled_region_indices, :]
         )
-        metrics.loc[col_name] = [
-            col_name,
-            adjusted_rand_score(labels, preds),
-            normalized_mutual_info_score(labels, preds),
-            f1_score(labels, preds, average="weighted"),
-            (silhouette_score(generalization_embeddings, labels) + 1)
-            * 0.5,  # Range [0-1] instead of [-1, 1]
-            _calc_entropy(preds_probs),
-        ]
+
+        if "AdjustedRandIndex" in metrics_list:
+            metrics.at[col_name, "AdjustedRandIndex"] = adjusted_rand_score(
+                labels, preds
+            )
+        if "NormalizedMutualInformation" in metrics_list:
+            metrics.at[col_name, "NormalizedMutualInformation"] = (
+                normalized_mutual_info_score(labels, preds)
+            )
+        if "F1Score" in metrics_list:
+            metrics.at[col_name, "F1Score"] = f1_score(
+                labels, preds, average="weighted"
+            )
+        if "SilhouetteCoefficient" in metrics_list:
+            metrics.at[col_name, "SilhouetteCoefficient"] = (
+                silhouette_score(generalization_embeddings, labels) + 1
+            ) * 0.5
+        if "MixingEntropyScore" in metrics_list:
+            metrics.at[col_name, "MixingEntropyScore"] = _calc_entropy(preds_probs)
 
     return metrics
